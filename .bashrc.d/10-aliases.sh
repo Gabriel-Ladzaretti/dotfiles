ac0a63c91f5e6191f3a6e0672a49e7267f93205e
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-02-20 19:05:31 +0200

    update bash aliases

--- .bashrc.d/10-aliases.sh
+++ .bashrc.d/10-aliases.sh
@@ -1,2 +1,2 @@
-alias config='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 alias xclip='xclip -selection c'
