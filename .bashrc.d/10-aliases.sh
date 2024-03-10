9c2ee0ead2383ba7b4da3c2514f867d2b3fa2ef6
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-03-10 09:31:39 +0200

    update tmux aliases

--- .bashrc.d/10-aliases.sh
+++ .bashrc.d/10-aliases.sh
@@ -1,3 +1,4 @@
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 alias xclip='xclip -selection c'
 alias tmux='tmux -u'
+alias tmuxx='tmux new-session -A -s'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,7 +35,7 @@ starship init fish | source
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 alias xclip='xclip -selection c'
 alias tmux='tmux -u'
-alias tmuxx='tmux new-session -A -s Home'
+alias tmuxx='tmux new-session -A -s'
 
 # if status is-interactive
 # and not set -q TMUX
