ade67db872c0df85eb51a9ffd8eb6bdf084d88ea
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-03-16 08:45:28 +0200

    fish: match terminal color scheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,25 +9,25 @@ set fish_greeting
 set -g fish_color_autosuggestion '555' 'brblack'
 set -g fish_color_cancel -r
 set -g fish_color_command --bold
-set -g fish_color_comment red
-set -g fish_color_cwd green
-set -g fish_color_cwd_root red
-set -g fish_color_end brmagenta
+set -g fish_color_comment '#F38BA8' # red
+set -g fish_color_cwd '#A6E3A1' # green
+set -g fish_color_cwd_root '#F38BA8' # red
+set -g fish_color_end '#F5C2E7' # brmagenta
 # set -g fish_color_error '#ff79a8' '--bold'
-set -g fish_color_error '#FFBF00' '--bold'
-set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_error '#F9E2AF' '--bold' # yellow
+set -g fish_color_escape '#F9E2AF'  '--bold' # yellow
 set -g fish_color_history_current --bold
 set -g fish_color_host normal
 set -g fish_color_match --background=brblue
 set -g fish_color_normal normal
-set -g fish_color_operator bryellow
+set -g fish_color_operator '#F9E2AF' # yellow
 # set -g fish_color_param cyan
-set -g fish_color_param cyan  '--bold'
-set -g fish_color_quote '#DFFF00'
-set -g fish_color_redirection brblue
-set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_param '#89DDFF' '--bold' # cyan
+set -g fish_color_quote '#DFFF00' # very bright greenish yellow
+set -g fish_color_redirection '#89B4FA' # blue
+set -g fish_color_search_match '#F9E2AF'  '--background=brblack' # fg=yellow
 set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-set -g fish_color_user brgreen
+set -g fish_color_user '#A6E3A1' #green
 set -g fish_color_valid_path --underline
 
 starship init fish | source
