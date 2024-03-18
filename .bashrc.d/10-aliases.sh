45794706d7eed5dda14343f809ba618f22d8da78
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-03-18 08:59:51 +0200

    add obsidian git alias

--- .bashrc.d/10-aliases.sh
+++ .bashrc.d/10-aliases.sh
@@ -1,4 +1,5 @@
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+alias obsidian='/usr/bin/git -C $HOME/GitHub/Obsidian'
 alias xclip='xclip -selection c'
 alias tmux='tmux -u'
 alias g="git"
