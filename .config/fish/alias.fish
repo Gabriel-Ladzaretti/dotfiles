70e007ceed9b144cbb14555b070726e480bedb5a
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-06-16 09:05:03 +0300

    add fish aliases

---
+++ .config/fish/alias.fish
@@ -0,0 +1,68 @@
+# Version control
+abbr -a g git
+abbr -a gs git status -sb
+abbr -a ga git add
+abbr -a gc git commit
+abbr -a gcm git commit -m
+abbr -a gca git commit --amend
+abbr -a gcl git clone
+abbr -a gco git checkout
+abbr -a gp git push
+abbr -a gpl git pull
+abbr -a gd git diff
+abbr -a gds git diff --staged
+abbr -a gf git fetch
+abbr -a gl git log --oneline -n 15
+
+# Dotfiles version control
+alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+abbr -a d dotfiles
+abbr -a ds dotfiles status -sb
+abbr -a da dotfiles add
+abbr -a dc dotfiles commit
+abbr -a dcm dotfiles commit -m
+abbr -a dca dotfiles commit --amend
+abbr -a dcl dotfiles clone
+abbr -a dco dotfiles checkout
+abbr -a dp dotfiles push
+abbr -a dpl dotfiles pull
+abbr -a dd dotfiles diff
+abbr -a dds dotfiles diff --staged
+abbr -a df dotfiles fetch
+abbr -a dl dotfiles log --oneline -n 15
+
+# Obsidian version control
+alias obsidian='/usr/bin/git -C $HOME/GitHub/Gabriel-Ladzaretti/obsidian'
+abbr -a o obsidian
+abbr -a os obsidian status -sb
+abbr -a oa obsidian add
+abbr -a oc obsidian commit
+abbr -a ocm obsidian commit -m
+abbr -a oct obsidian commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
+abbr -a oca obsidian commit --amend
+abbr -a ocl obsidian clone
+abbr -a oco obsidian checkout
+abbr -a op obsidian push
+abbr -a opl obsidian pull
+abbr -a od obsidian diff
+abbr -a ods obsidian diff --staged
+abbr -a of obsidian fetch
+abbr -a ol obsidian log --oneline -n 15
+
+# Copy to clipboard
+alias xclip='xclip -selection c'
+
+# Tmux shortcuts
+alias tmux='tmux -u'
+alias tmuxx='tmux new-session -A -s'
+
+# Navigation
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias cd..='cd ..' # Address cd typo
+
+# Code with wayland enabled
+alias code='code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto --unity-launch $argv  > /dev/null 2>&1'
+
+# misc
+alias reload='exec fish'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,69 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# Disable intro greeting
-set fish_greeting
-
-# Fish syntax highlighting
-set -g fish_color_autosuggestion '555' 'brblack'
-set -g fish_color_cancel -r
-set -g fish_color_command --bold
-set -g fish_color_comment '#F38BA8' # red
-set -g fish_color_cwd '#A6E3A1' # green
-set -g fish_color_cwd_root '#F38BA8' # red
-set -g fish_color_end '#F5C2E7' # brmagenta
-# set -g fish_color_error '#ff79a8' '--bold'
-set -g fish_color_error '#F9E2AF' '--bold' # yellow
-set -g fish_color_escape '#F9E2AF'  '--bold' # yellow
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator '#F9E2AF' # yellow
-# set -g fish_color_param cyan
-set -g fish_color_param '#89DDFF' '--bold' # cyan
-set -g fish_color_quote '#DFFF00' # very bright greenish yellow
-set -g fish_color_redirection '#89B4FA' # blue
-set -g fish_color_search_match '#F9E2AF'  '--background=brblack' # fg=yellow
-set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-set -g fish_color_user '#A6E3A1' #green
-set -g fish_color_valid_path --underline
-
-starship init fish | source
-
-# Dotfiles version control
-alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
-alias d='dotfiles'
-
-# Obsidian
-alias obsidian='/usr/bin/git -C $HOME/GitHub/Gabriel-Ladzaretti/obsidian'
-alias o=obsidian
-
-# Copy to clipboard
-alias xclip='xclip -selection c'
-
-# Tmux shortcuts
-alias tmux='tmux -u'
-alias tmuxx='tmux new-session -A -s'
-
-# General VCS
-alias g='git'
-
-# Directory navigation
-alias ...='cd ../..'
-alias ....='cd ../../..'
-# Address cd typo
-alias cd..='cd ..'
-alias docs='cd ~/Documents'
-alias dl='cd ~/Downloads'
-
-alias code='code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=auto --unity-launch $argv  > /dev/null 2>&1'
-
+source ~/.config/fish/alias.fish
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    #
    
     # Function to display fastfetch welcome screen 
     # if running in alacritty terminal
@@ -78,3 +16,32 @@ if status is-interactive
     alacritty_welcome
 end
 
+# Disable intro greeting
+set fish_greeting
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555' 'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment '#F38BA8' # red
+set -g fish_color_cwd '#A6E3A1' # green
+set -g fish_color_cwd_root '#F38BA8' # red
+set -g fish_color_end '#F5C2E7' # brmagenta
+set -g fish_color_error '#F9E2AF' '--bold' # yellow
+set -g fish_color_escape '#F9E2AF'  '--bold' # yellow
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator '#F9E2AF' # yellow
+set -g fish_color_param '#89DDFF' '--bold' # cyan
+set -g fish_color_quote '#DFFF00' # very bright greenish yellow
+set -g fish_color_redirection '#89B4FA' # blue
+set -g fish_color_search_match '#F9E2AF'  '--background=brblack' # fg=yellow
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user '#A6E3A1' #green
+set -g fish_color_valid_path --underline
+# set -g fish_color_error '#ff79a8' '--bold'
+# set -g fish_color_param cyan
+
+starship init fish | source
